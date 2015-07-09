f03a4ee1fc7d0123666ebce097d1c5375d299c04
Nikos Zinas <zinas.nikos@gmail.com>
2015-07-09 07:08:38 +0200

    restyled area information list of monsters and characters

--- assets/jsx/components/area-information.jsx
+++ assets/jsx/components/area-information.jsx
@@ -18,6 +18,21 @@ module.exports = React.createClass({
     }).bind(this));
   },
 
+  getButtonClass: function (level) {
+    var className = 'btn btn-xs ';
+    var diff = this.props.character.level - level;
+    if ( diff > 3 ) {
+      className += 'btn-default';
+    } else if ( diff > -1 ) {
+      className += 'btn-success';
+    } else if ( diff > -3 ) {
+      className += 'btn-warning';
+    } else {
+      className += 'btn-danger';
+    }
+    return className;
+  },
+
   render: function() {
     return (
       <div className="panel panel-danger">
@@ -31,23 +46,30 @@ module.exports = React.createClass({
           <p>{this.props.level.description}</p>
           {this.props.area.monsters.map( (function (monster) {
             return (
-              <a
-                key={monster.id}
-                className="btn btn-danger btn-rounded btn-attack"
-                onClick={this.fight}
-                data-target="monster"
-                data-id={monster.id}>
-                <span className="fa fa-bug"></span> {monster.name} (lvl. {monster.level})
-              </a>
+              <p>
+                <a
+                  key={monster.id}
+                  className={this.getButtonClass(monster.level)}
+                  onClick={this.fight}
+                  data-target="monster"
+                  data-id={monster.id}>
+                  <span className="fa fa-bug"></span> {monster.name} (lvl. {monster.level})
+                </a>
+              </p>
             )
           }).bind(this))}
-          {this.props.area.characters.map(function (character) {
+          {this.props.area.characters.map((function (character) {
             return (
-              <a key={character.id} className="btn btn-danger btn-rounded" href={'/game/fight/character/'+character.id}>
-                <span className="fa fa-male"></span> {character.name} (lvl. {character.level})
-              </a>
+              <p>
+                <a
+                  key={character.id}
+                  className={this.getButtonClass(character.level)}
+                  href={'/game/fight/character/'+character.id}>
+                  <span className="fa fa-male"></span> {character.name} (lvl. {character.level})
+                </a>
+              </p>
             )
-          })}
+          }).bind(this))}
         </div>
       </div>
     );
